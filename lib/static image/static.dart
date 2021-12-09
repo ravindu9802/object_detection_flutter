48aa633a3da9d78a10284672da4959bbfa2fb9e4
ravindu9802 <ravindurasanjana1998@gmail.com>
2021-12-09 23:19:55 +0530

    initial commit

---
+++ lib/static image/static.dart
@@ -0,0 +1,226 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class StaticImage extends StatefulWidget {
+  @override
+  _StaticImageState createState() => _StaticImageState();
+}
+
+class _StaticImageState extends State<StaticImage> {
+  File _image;
+  List _recognitions;
+  bool _busy;
+  double _imageWidth, _imageHeight;
+  bool isSpeaking = false;
+  List<Widget> stackChildren = [];
+  final picker = ImagePicker();
+  final _flutterTts = FlutterTts();
+
+  // this function loads the model
+  loadTfModel() async {
+    await Tflite.loadModel(
+      model: "assets/models/ssd_mobilenet.tflite",
+      labels: "assets/models/labels.txt",
+    );
+  }
+
+  //initalize flutter_tts
+  void initializeTts() {
+    _flutterTts.setStartHandler(() {
+      setState(() {
+        isSpeaking = true;
+      });
+    });
+    _flutterTts.setCompletionHandler(() {
+      setState(() {
+        isSpeaking = false;
+      });
+    });
+    _flutterTts.setErrorHandler((message) {
+      setState(() {
+        isSpeaking = false;
+      });
+    });
+  }
+
+  // this function detects the objects on the image
+  detectObject(File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+        path: image.path, // required
+        model: "SSDMobileNet",
+        imageMean: 127.5,
+        imageStd: 127.5,
+        threshold: 0.4, // defaults to 0.1
+        numResultsPerClass: 10, // defaults to 5
+        asynch: true // defaults to true
+        );
+    FileImage(image)
+        .resolve(ImageConfiguration())
+        .addListener((ImageStreamListener((ImageInfo info, bool _) {
+          setState(() {
+            _imageWidth = info.image.width.toDouble();
+            _imageHeight = info.image.height.toDouble();
+            _recognitions = recognitions;
+
+            stackChildren.clear();
+            stackChildren.add(Positioned(
+              // using ternary operator
+              child: _image == null
+                  ? Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Please Select an Image"),
+                  ],
+                ),
+              )
+                  : // if not null then
+              Container(child: Image.file(_image)),
+            ));
+
+            stackChildren.addAll(renderBoxes(MediaQuery.of(context).size));
+
+            if (_busy) {
+              stackChildren.add(Center(
+                child: CircularProgressIndicator(),
+              ));
+            }
+          });
+        })));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _busy = true;
+    initializeTts();
+    speak("speaking");
+    loadTfModel().then((val) {
+      {
+        setState(() {
+          _busy = false;
+        });
+      }
+    });
+  }
+
+  void speak(String word) async {
+    print(word);
+    if (word.isNotEmpty) {
+      await _flutterTts.speak(word);
+    }
+  }
+
+  void stop() async {
+    await _flutterTts.stop();
+  }
+
+  @override
+  void dispose() {
+    _flutterTts.stop();
+    super.dispose();
+  }
+
+  // display the bounding boxes over the detected objects
+  List<Widget> renderBoxes(Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageWidth == null || _imageHeight == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageWidth * screen.width;
+
+    Color blue = Colors.blue;
+
+print(_recognitions);
+    for (dynamic re in _recognitions) {
+      speak(re["detectedClass"].toString());
+    }
+    return _recognitions.map((re) {
+      return Container(
+        child: Positioned(
+            left: re["rect"]["x"] * factorX,
+            top: re["rect"]["y"] * factorY,
+            width: re["rect"]["w"] * factorX,
+            height: re["rect"]["h"] * factorY,
+            child: ((re["confidenceInClass"] > 0.50))
+                ? Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: blue,
+                      width: 3,
+                    )),
+                    child: Text(
+                      "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+                      style: TextStyle(
+                        background: Paint()..color = blue,
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                  )
+                : Container()),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Object Detector"),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            heroTag: "Fltbtn2",
+            child: Icon(Icons.camera_alt),
+            onPressed: getImageFromCamera,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            heroTag: "Fltbtn1",
+            child: Icon(Icons.photo),
+            onPressed: getImageFromGallery,
+          ),
+        ],
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          children: stackChildren,
+        ),
+      ),
+    );
+  }
+
+  // gets image from camera and runs detectObject
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      _image = File(pickedFile.path);
+      detectObject(_image);
+    } else {
+      print("No image Selected");
+    }
+  }
+
+  // gets image from gallery and runs detectObject
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _image = File(pickedFile.path);
+      detectObject(_image);
+    } else {
+      print("No image Selected");
+    }
+
+  }
+}
